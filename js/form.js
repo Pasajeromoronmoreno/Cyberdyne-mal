31787ca6c6f6c42c516edb648f9d023d306cb03f
Pasajeromoronmoreno <pasajeromoronmoreno@gmail.com>
2016-08-11 23:39:11 -0300

    Login con javascript y return false

--- js/form.js
+++ js/form.js
@@ -3,20 +3,20 @@
 function validar () {
   var user = document.querySelector("#user");
   var pass = document.querySelector("#pass");
-    if (user.value === "ariel" && pass.value === "comunidadit")  { 
+    if (user.value === "ariel" && pass.value === "comunidadit")  {
   console.log("Correcto!" + "\n" + "Username:" + user.value + "\n" + "Password:" + pass.value);
     window.location.href = "loged-in.html";
   // form.submit
     } else {
-  console.log("No esta autorizado!" + "\n" + "Username:" + user.value + "\n" + "Password:" + pass.value);    
+  console.log("No esta autorizado!" + "\n" + "Username:" + user.value + "\n" + "Password:" + pass.value);
     }
 };
 
 
 // $(function () {
-//     $("#sub").click(function (e) { 
+//     $("#sub").click(function (e) {
 //         e.preventDefault();
-//         if (user.value === "ariel" && pass.value === "comunidadit")  { 
+//         if (user.value === "ariel" && pass.value === "comunidadit")  {
 //   console.log("Correcto!" + "\n" + "Username:" + user.value + "\n" + "Password:" + pass.value);
 //     window.location.href = "loged-in.html";
 //      } else {
@@ -28,12 +28,12 @@ function validar () {
 
 
 
-// $(function () {    
+// $(function () {
 //      var $sub = $("#sub")
-//      $("#sub").click(function (e) {         
-//          console.log("anda");         
+//      $("#sub").click(function (e) {
+//          console.log("anda");
 //      });
-// });   
+// });
 
 
 
@@ -43,10 +43,10 @@ function validar () {
 // $(document).ready(function () {
 //     $('.forgot-pass').click(function(event) {
 //       $(".pr-wrap").toggleClass("show-pass-reset");
-//     }); 
-    
+//     });
+
 //     $('.pass-reset-submit').click(function(event) {
 //       $(".pr-wrap").removeClass("show-pass-reset");
-//     }); 
+//     });
 // });
 
